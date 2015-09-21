b3f172b5508206c1b313953f871462a4a3e01fa7
Vladislav Chebotaryov <cvl@ciklums-mini.kyiv.ciklum.net>
2015-09-21 14:45:38 +0300

    [refactoring] - Rewrite algoritm for returning template files

--- opendomains/views/views.py
+++ opendomains/views/views.py
@@ -1,5 +1,5 @@
 import os
-from flask import send_from_directory, render_template
+from flask import send_from_directory, render_template, request
 
 __author__ = 'cvl'
 
@@ -9,23 +9,11 @@ from opendomains import app
 def index():
     return render_template("index.html")
 
-@app.route('/<path:url_path>')
-def content(url_path):
-    length = len(url_path) - 1
-
-    file_name = ''
-    path = ''
-    while 1:
-        if not (url_path[length] == '/') | (length == 0):
-            file_name = file_name[:0] + url_path[length] + file_name[0:]
-            length -= 1
-        else:
-            break
-    path = url_path.replace(file_name, '')
-
+@app.route('/<path:path>/<file>')
+def dynamic_files(path, file):
     abs_path = os.path.dirname(os.path.abspath(__file__))
     od_path = abs_path.replace('/' + os.path.basename(abs_path), '')
-
     path_with_file = od_path + '/templates/' + path
-    response = send_from_directory(path_with_file, file_name, as_attachment=True)
+
+    response = send_from_directory(path_with_file, file, as_attachment=True)
     return response
